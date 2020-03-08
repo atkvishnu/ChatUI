1015b38f0a6dfd541af9fe4e3d68def6d0b9fc4a
nerdyninjahugs <nerdyninjahugs4u@gmail.com>
2020-03-09 01:33:57 +0530

    Adding build files

---
+++ build/precache-manifest.e35ca504cb649a51279ef0c8f8f8a24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c413477fb52719ea0b17f3796dc9074d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6a1c38c026236a3fae",
+    "url": "/static/css/main.8d82d99e.chunk.css"
+  },
+  {
+    "revision": "356f80c56128c9a297d1",
+    "url": "/static/js/2.67a7735c.chunk.js"
+  },
+  {
+    "revision": "66f097c4048c78bd2ca4c7252648ce62",
+    "url": "/static/js/2.67a7735c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6a1c38c026236a3fae",
+    "url": "/static/js/main.d97fdcbf.chunk.js"
+  },
+  {
+    "revision": "bd7acc51af391bc1acca",
+    "url": "/static/js/runtime-main.1c8cc441.js"
+  },
+  {
+    "revision": "0f27710fa38afad4bb13b9420ce75024",
+    "url": "/static/media/laptop.0f27710f.webp"
+  }
+]);
--- src/components/newchat/newChat.js
+++ src/components/newchat/newChat.js
@@ -5,113 +5,123 @@ const firebase = require("firebase");
 
 class NewChatComponent extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      username: null,
+      message: null
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            username: null,
-            message: null
-        }
+  render() {
+
+    const { classes } = this.props;
+
+    return(
+      <main className={classes.main}>
+        <CssBaseline/>
+        <Paper className={classes.paper}>
+          <Typography component="h1" variant="h5">Send A Message!</Typography>
+          <form className={classes.form} onSubmit={(e) => this.submitNewChat(e)}>
+            <FormControl fullWidth>
+              <InputLabel htmlFor='new-chat-username'>
+                  Enter Your Friend's Email
+              </InputLabel>
+              <Input required 
+                className={classes.input}
+                autoFocus 
+                onChange={(e) => this.userTyping('username', e)} 
+                id='new-chat-username'>
+              </Input>
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel htmlFor='new-chat-message'>
+                  Enter Your Message
+              </InputLabel>
+              <Input required 
+                className={classes.input}
+                onChange={(e) => this.userTyping('message', e)} 
+                id='new-chat-message'>
+              </Input>
+            </FormControl>
+            <Button fullWidth variant='contained' color='primary' className={classes.submit} type='submit'>Send</Button>
+          </form>
+          {
+            this.state.serverError ? 
+            <Typography component='h5' variant='h6' className={classes.errorText}>
+              Unable to locate the user
+            </Typography> :
+            null
+          }
+        </Paper>
+      </main>
+    );
+  }
+
+  componentWillMount() {
+    if(!firebase.auth().currentUser)
+      this.props.history.push('/login');
+  }
+
+  userTyping = (inputType, e) => {
+    switch (inputType) {
+      case 'username':
+        this.setState({ username: e.target.value });
+        break;
+      
+      case 'message':
+        this.setState({ message: e.target.value });
+        break;
+
+      default:
+        break;
     }
+  }
 
-
-    render() {
-
-        const { classes } = this.props;
-
-        return(
-            <main className={classes.main}>
-                <CssBaseline></CssBaseline>
-                <Paper className={classes.paper}>
-                    <Typography component='h1' variant='h5'>Send A Message!</Typography>
-                    <form className={classes.form} onSubmit={(e) => this.submitNewChat(e)}>
-                        <FormControl fullWidth>
-                        <InputLabel htmlFor="new-chat-username">
-                            Enter your Friend's Email
-                        </InputLabel>
-                        <Input required
-                            className={classes.input}
-                            autoFocus
-                            onChange={(e) => this.userTyping('username', e)}
-                            id='new-chat-username'></Input>
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="new-chat-message">
-                                Enter your Message
-                            </InputLabel>
-                            <Input required 
-                                className={classes.input}
-                                onChange={(e) => this.userTyping('message', e)}
-                                id='new-chat-message'></Input>
-                        </FormControl>
-                        <Button fullWidth className={classes.submit} variant="contained" color="primary" type="submit">Send!</Button>
-                    </form>
-                </Paper>
-            </main>
-        );
+  submitNewChat = async (e) => {
+    e.preventDefault();
+    const userExists = await this.userExists();
+    if(userExists) {
+      const chatExists = await this.chatExists();
+      chatExists ? this.goToChat() : this.createChat();
     }
+  }
 
-    userTyping = (type, e) => {
-        switch(type) {
-            case 'username':
-                this.setState({ username: e.target.value });
-                break;
-            
-            case 'message':
-                this.setState({ message: e.target.value });
-                break;
+  buildDocKey = () => [firebase.auth().currentUser.email, this.state.username].sort().join(':');
 
-            default:
-                break;
-        }
-    }
+  createChat = () => {
+    this.props.newChatSubmitFn({
+      sendTo: this.state.username,
+      message: this.state.message
+    });
+  }
 
-    submitNewChat = async (e) => {
-        e.preventDefault();
-        const userExists = await this.userExists();
-        if(userExists) {
-            const chatExists = await this.chatExists();
-            chatExists ? this.goToChat() : this.createChat();
-        }
-    }
+  goToChat = () => this.props.goToChatFn(this.buildDocKey(), this.state.message);
 
-    createChat = () => {
-        this.props.newChatSubmitFn({
-            sendTo: this.state.username,
-            message: this.state.message
-        });
-    }
-
-    goToChat = () => this.props.goToChatFn(this.buildDocKey(), this.state.message);
-
-    buildDocKey = () => {
-        return [firebase.auth().currentUser.email, this.state.username].sort().join(':');
-    }
-
-    chatExists = async () => {
-        const docKey = this.buildDocKey();
-        const chat = await firebase
-            .firestore()
-            .collection('chats')
-            .doc(docKey)
-            .get();
-        console.log(chat.exists);
-        return chat.exists;
-            
-    }
-
-    userExists = async () => {
-        const usersSnapshot = await firebase
-            .firestore()
-            .collection('users')
-            .get();
-        const exists = usersSnapshot.docs
-            .map(_doc => _doc.data().email)
-            .includes(this.state.username);
-        return exists;
-    }
-
-
+  chatExists = async () => {
+    const docKey = this.buildDocKey();
+    const chat = await 
+      firebase
+      .firestore()
+      .collection('chats')
+      .doc(docKey)
+      .get();
+    console.log(chat.exists);
+    return chat.exists;
+  }
+  userExists = async () => {
+    const usersSnapshot = await 
+    firebase
+      .firestore()
+      .collection('users')
+      .get();
+    const exists = usersSnapshot
+      .docs
+        .map(_doc => _doc.data().email)
+        .includes(this.state.username);
+    this.setState({ serverError: !exists });
+    return exists;
+  }
 }
 
 export default withStyles(styles)(NewChatComponent);
